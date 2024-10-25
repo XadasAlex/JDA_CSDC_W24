695f9881efe411b5f69f3d4e7e37788703198786
Alex Gebhart <hattie2908@gmail.com>
2024-10-25 14:48:59 +0200

    moved launcher to its own package

--- src/main/java/launcher/Launcher.java
+++ src/main/java/launcher/Launcher.java
@@ -1,4 +1,7 @@
+package launcher;
+
 import api.SimpleHttpServer;
+import api.helper.JDAHelper;
 import bot.utils.ErrorHandler;
 import bot.utils.MessageAnalyzer;
 import handlers.CommandHandler;
@@ -7,6 +10,7 @@ import listeners.ReadyListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.openjfx.MyApp;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,9 +20,7 @@ import java.util.EnumSet;
 
 
 public class Launcher {
-
-
-    public static void main(String[] args) throws InterruptedException {
+    public static JDA initJDA() {
         final String TOKEN = getAuthToken();
         final EnumSet<GatewayIntent> INTENTS = createIntents();
         final String COMMAND_PREFIX = "!";
@@ -27,15 +29,16 @@ public class Launcher {
         CommandHandler ch = new CommandHandler();
         ErrorHandler eh = new ErrorHandler();
 
-
-
-        JDA jda = JDABuilder
+        return JDABuilder
                 .createDefault(TOKEN)
                 .enableIntents(INTENTS)
                 .addEventListeners(new MessageListener(ma, ch, eh))
                 .addEventListeners(new ReadyListener())
                 .build();
+    }
 
+    public static void start(JDA jda) throws InterruptedException {
+        /*
         Thread httpThread = new Thread(() -> {
             SimpleHttpServer server = new SimpleHttpServer(jda);
             try {
@@ -45,11 +48,18 @@ public class Launcher {
             }
         });
         httpThread.start();
-
+        */
 
         jda.awaitReady();
-
     }
+
+    public static void main(String[] args) throws InterruptedException {
+        JDA jda = initJDA();
+        JDAHelper.setJDA(jda);
+        start(jda);
+        MyApp.launch(MyApp.class, args);
+    }
+
 
     public static EnumSet<GatewayIntent> createIntents() {
         return EnumSet.of(
