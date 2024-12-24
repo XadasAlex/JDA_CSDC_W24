9028fb3b3e3fc8c22756fa3b34ed0349a740f26a
Alex Gebhart <hattie2908@gmail.com>
2024-12-24 03:07:45 +0100

    select try - will be progressed later (not important yet)

---
+++ src/main/java/listeners/SelectListener.java
@@ -0,0 +1,13 @@
+package listeners;
+
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class SelectListener extends ListenerAdapter {
+    @Override
+    public void onStringSelectInteraction(StringSelectInteractionEvent event) {
+
+    }
+
+
+}
---
+++ src/main/java/utils/SelectMenu.java
@@ -0,0 +1,9 @@
+package utils;
+
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
+
+public class SelectMenu {
+    public static StringSelectMenu createStringSelectMenu(String name) {
+        return null;
+    }
+}
