f0c60a4785999eb809ce79ac48e6a0e3076f1f90
Alex Gebhart <hattie2908@gmail.com>
2024-12-24 02:57:41 +0100

    created a Leaderboard Command
    with this command the leaderboard can be displayed of all the members that allowed stat tracking - sorted by the exp collected of each member.

---
+++ src/main/java/commands/chat/CmdLeaderBoard.java
@@ -0,0 +1,61 @@
+package commands.chat;
+
+import commands.ICommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import stats.MemberStats;
+import utils.MessageSender;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class CmdLeaderBoard implements ICommand {
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        EmbedBuilder leaderBoardEmbed = createLeaderBoardEmbed(event);
+        if (leaderBoardEmbed != null) {
+            event.replyEmbeds(leaderBoardEmbed.build()).queue();
+        }
+    }
+
+    private EmbedBuilder createLeaderBoardEmbed(SlashCommandInteractionEvent event) {
+        List<MemberStats> memberStatsList = MemberStats.topMembers(event.getGuild().getId());
+
+        if (memberStatsList != null) {
+            memberStatsList.sort(Comparator.comparingInt(MemberStats::getExp));
+            if (memberStatsList.size() > 10) {
+                memberStatsList.subList(0, 10);
+            }
+            EmbedBuilder leaderBoardEmbed = MessageSender.createEmbedBlueprint(event, "title", "commandname", "description");
+            for (MemberStats stats : memberStatsList) {
+                Member member = event.getGuild().getMemberById(stats.getMemberId());
+                if (member == null) continue;
+                leaderBoardEmbed.addField(member.getEffectiveName(), Integer.toString(stats.getExp()), true);
+            }
+
+            return leaderBoardEmbed;
+
+
+        } else {
+            return null;
+        }
+
+    }
+
+    @Override
+    public String getName() {
+        return "leaderboard";
+    }
+
+    @Override
+    public String getDescription() {
+        return "shows the leaderboard more with: ?leaderboard";
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return List.of();
+    }
+}
