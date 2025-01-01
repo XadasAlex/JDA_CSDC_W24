1a10b2381f4424a423744fdf45c47f0ea9a4e984
Alex Gebhart <hattie2908@gmail.com>
2025-01-01 16:57:10 +0100

    added icons for embeds (see: Embedder)

---
+++ src/main/java/utils/CommandIcons.java
@@ -0,0 +1,26 @@
+package utils;
+
+public class CommandIcons {
+    public static final String STATS_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321250730820243496/icons8-daten-96_2.png?ex=676c8e21&is=676b3ca1&hm=9815f3b99c0b62bf2315521a3ebb5af5fd49458b5ae0b484fe0c27136b04516b&";
+    public static final String STATS_ERROR_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321250731000594462/icons8-daten-96_3.png?ex=676c8e21&is=676b3ca1&hm=26891f1f43ca83432eb67617527297b527ef728efdf3cc5a30b6611fc18230f8&";
+
+    public static final String CHAT_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321114420084080701/icons8-unterhaltung-96.png?ex=676c0f2e&is=676abdae&hm=605e1990d80ad7ca3bbfd16538669c667cbebd763be3af9e9239802eeb4bd6d4&";
+    public static final String CHAT_ERROR_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321250731751243896/icons8-unterhaltung-96_1.png?ex=676c8e22&is=676b3ca2&hm=9ed30e1de9ba0331beb96e95cf16cc10a1e7d9f648513c4cafaceb940e6f4672&";
+
+    public static final String SETTINGS_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321114671478210560/icons8-einstellungen-96.png?ex=676c0f6a&is=676abdea&hm=e26529d0370df7d6a7bc619c11325eaf4155eee12bd3936a8605f5cbc2dad116&";
+    public static final String SETTINGS_ERROR_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321250731268898868/icons8-einstellungen-96_1.png?ex=676c8e21&is=676b3ca1&hm=7ae5b0b2925238fb4d05c38f9b548374cc57f04ae3c920d0432e8cc80ad6be55&";
+
+    public static final String COMMUNITY_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321250903096954951/icons8-konferenztelefonat-96_1.png?ex=676c8e4a&is=676b3cca&hm=724c52e64bb72d130ab34e8dab0ad957ad10c3daccf94b7316162e6243d18b49&";
+    public static final String COMMUNITY_ERROR_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321250903331831922/icons8-konferenztelefonat-96.png?ex=676c8e4b&is=676b3ccb&hm=6413b916af8d7139870361b63b19a1ff84643f69e1872d929a677b492b9efac6&";
+
+    public static final String LIGHTNING_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321115094440218624/icons8-blitz-96.png?ex=676c0fcf&is=676abe4f&hm=0159d4a7e9d95ce41c7d96ada8c48ede68b0cd7e3f1d2087d71747ed5da5e10b&";
+    public static final String LIGHTNING_ERROR_ICON_URL = "https://cdn.discordapp.com/attachments/813431472446898189/1321121622203039785/icons8-blitz-96_1.png?ex=676c15e3&is=676ac463&hm=11ef8f927a19ddfc9901b9ae893a11a5fbec687084e7bd46e27f01cb8aeb6d18&";
+
+    public static final String CHAT_GPT_ICON = "https://imgs.search.brave.com/nwhwzHT-xTYl4Ni6QTC8rA7eMxCHjxyAVylDaeHyIK0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kb3du/bG9hZHMuaW50ZXJj/b21jZG4uY29tL2kv/by83NDMyMTM5NzIv/N2VhYmJmNTJjYWNh/Yzg4NTEzM2NhNzEy/L2ljb24taW9zLTEw/MjRAMXgucG5n";
+
+    public static final String OCTAEDER_ICON = "https://cdn.discordapp.com/attachments/813431472446898189/1321305425932587088/icons8-oktaeder-96.png?ex=676cc112&is=676b6f92&hm=fb5241cba90349f97b7613bf5d64a61a6b39032fe566c891aa148781f90ab6f2&";
+    public static final String GAME_ICON = "https://cdn.discordapp.com/attachments/813431472446898189/1321305425655894046/icons8-brettspielfigur-96.png?ex=676cc112&is=676b6f92&hm=d3dc5c46da02e1ca59355d2399e1a61a15b1f9fcf348d2ab445317847496d7fa&";
+    public static final String DICE_ICON = "https://cdn.discordapp.com/attachments/813431472446898189/1321306475934781571/icons8-wurfel-96.png?ex=676cc20c&is=676b708c&hm=0b7da586f30873671926a47d8f772363b22883e747483193ce6ce073612c53fa&";
+
+    public static final String ADMIN_ICON = "https://cdn.discordapp.com/attachments/813431472446898189/1321454697415970846/icons8-hammer-und-amboss-96.png?ex=676d4c17&is=676bfa97&hm=dcc2144f3fc2d2286fcfed7cb8cbfe71035e9494719b50897957c9b1ba0ee3ba&";
+}
