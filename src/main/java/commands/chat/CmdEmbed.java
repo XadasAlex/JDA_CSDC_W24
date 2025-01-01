2669bf1cc2c57efed2b418e054627d2e5f4ec1d3
Alex Gebhart <hattie2908@gmail.com>
2025-01-01 16:47:16 +0100

    testing purposes

--- src/main/java/commands/chat/CmdEmbed.java
+++ src/main/java/commands/chat/CmdEmbed.java
@@ -1,16 +1,12 @@
 package commands.chat;
 
-import launcher.Bot;
 import commands.ICommand;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.SelfUser;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import utils.Helper;
+import utils.Embedder;
 
 import java.util.List;
-import java.util.Set;
 
 public class CmdEmbed implements ICommand {
     @Override
@@ -35,17 +31,10 @@ public class CmdEmbed implements ICommand {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        EmbedBuilder embed = new EmbedBuilder();
-        Bot botInstance = Bot.getInstance();
-        SelfUser botUser = botInstance.getJda().getSelfUser();
-        Member author = event.getMember();
-
-        embed.setTitle("test", botInstance.getAvatarUrl());
-        embed.setAuthor(botUser.getEffectiveName(), botUser.getEffectiveAvatarUrl(), botUser.getEffectiveAvatarUrl());
-        embed.setColor(botInstance.getDefaultColor());
-        embed.setDescription("description test");
-        embed.setFooter(String.format("Requested by: %s", author.getEffectiveName()), author.getEffectiveAvatarUrl());
-
-        event.replyEmbeds(embed.build()).queue();
+        event.replyEmbeds(Embedder.createBaseErrorEmbed(event.getMember(), "stats").build()).queue(
+                msg -> {
+                    Helper.deleteAfter(msg, 3);
+                }
+        );
     }
 }
