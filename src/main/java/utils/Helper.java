fb9bfd0a0dcf0b69a634722533a7804a947b2d23
Alex Gebhart <hattie2908@gmail.com>
2025-01-01 17:02:18 +0100

    added and removed functionality
    removed:
    getTaggedMember was purely for message commands with prefixes, using slash commands removes this complexity by adding options - so this got removed
    
    as well as getAudioChannelFromString (idk, seemd stupid)
    
    added:
    deleteAfter
    
    after queuing a message e.g.: event.reply("message").queue()
    you queue (non-blocking async operation), queue has a callback - and into this callback we'll pass a static reference to the helper method deleteAfter, this receives an interactionHook which fetches the original message (sent) and deletes it after some time (using the singleThreadScheduledExecutor)

--- src/main/java/utils/Helper.java
+++ src/main/java/utils/Helper.java
@@ -2,11 +2,13 @@ package utils;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.Channel;
 import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -18,11 +20,13 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Helper {
-    private static List<String> activePolls = new ArrayList<>();
+
     private static String[] numberEmojis = {
             "\u0031\uFE0F\u20E3", "\u0032\uFE0F\u20E3", "\u0033\uFE0F\u20E3", "\u0034\uFE0F\u20E3",
             "\u0035\uFE0F\u20E3", "\u0036\uFE0F\u20E3", "\u0037\uFE0F\u20E3", "\u0038\uFE0F\u20E3",
@@ -72,14 +76,6 @@ public class Helper {
     }
 
 
-    public static Member getTaggedMember(MessageReceivedEvent event) {
-        return getTaggedMember(event.getGuild(), event.getMessage().getContentRaw());
-    }
-
-    public static List<Member> getTaggedMembers(MessageReceivedEvent event) {
-        return getTaggedMembers(event.getGuild(), event.getMessage().getContentRaw());
-    }
-
 
     public static long getIdFromString(String content) {
         Pattern pattern = Pattern.compile("<@(\\d+)>");
@@ -116,31 +112,6 @@ public class Helper {
         }
 
         return output.toString();
-    }
-
-    public static AudioChannel getAudioChannelFromString(String content, Guild guild, MessageChannel msgChannel) {
-
-        long channelId = getIdFromString(content);
-        Channel channel = guild.getGuildChannelById(channelId);
-
-        if (channel == null) {
-            ErrorSender.notFoundError(msgChannel, "Channel with ID " + channelId + " does not exist.");
-            return null;
-        }
-
-        if (channel instanceof AudioChannel) {
-            return (AudioChannel) channel;
-        } else {
-            ErrorSender.notFoundError(msgChannel, "Channel with ID " + channelId + " is not an AudioChannel.");
-            return null;
-        }
-    }
-
-    public static AudioChannel getAudioChannelFromMessage(MessageReceivedEvent event) {
-        String content = event.getMessage().getContentRaw();
-        Guild guild = event.getGuild();
-
-        return getAudioChannelFromString(content, guild, event.getChannel());
     }
 
     public static List<Long> getIdsFromString(String content) {
@@ -236,4 +207,29 @@ public class Helper {
         return String.format("%d year%s ago.", years, sFormatting(years));
     }
 
+    public static void deleteAfter15(InteractionHook message) {
+        deleteAfter(message, 15);
+    }
+
+    public static void deleteAfter30(InteractionHook message) {
+        deleteAfter(message, 30);
+    }
+
+    public static void deleteAfter60(InteractionHook message) {
+        deleteAfter(message, 60);
+    }
+
+    public static void deleteAfter300(InteractionHook message) {
+        deleteAfter(message, 300);
+    }
+
+    public static void deleteAfter600(InteractionHook message) {
+        deleteAfter(message, 600);
+    }
+
+    public static void deleteAfter(InteractionHook message, int seconds) {
+        Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+            message.deleteOriginal().queue();
+        }, seconds, TimeUnit.SECONDS);
+    }
 }
