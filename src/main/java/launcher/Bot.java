8f645a6296aff03e2679d81eb7253be19ee7f0ac
Alex Gebhart <hattie2908@gmail.com>
2025-02-02 14:50:44 +0100

    implemented lavalink handling

--- src/main/java/launcher/Bot.java
+++ src/main/java/launcher/Bot.java
@@ -7,10 +7,29 @@ import commands.chat.stats.CmdAllowStats;
 import commands.chat.stats.CmdLeaderBoard;
 import commands.chat.stats.CmdStats;
 import commands.guild.CmdGuildInfo;
+import commands.guild.CmdJoin;
+import commands.guild.CmdLeave;
 import commands.guild.CmdSettingsChatRestricted;
+import commands.music.CmdPause;
 import commands.music.CmdPlay;
+import commands.music.CmdResume;
+import commands.music.CmdStop;
+import dev.arbjerg.lavalink.client.*;
+import dev.arbjerg.lavalink.client.event.EmittedEvent;
+import dev.arbjerg.lavalink.client.event.ReadyEvent;
+import dev.arbjerg.lavalink.client.event.StatsEvent;
+import dev.arbjerg.lavalink.client.event.TrackStartEvent;
+import dev.arbjerg.lavalink.client.loadbalancing.RegionGroup;
+import dev.arbjerg.lavalink.client.loadbalancing.builtin.VoiceRegionPenaltyProvider;
+import dev.arbjerg.lavalink.client.player.Track;
+import dev.arbjerg.lavalink.libraries.jda.JDAVoiceUpdateListener;
 import listeners.*;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import okhttp3.ResponseBody;
 import utils.GuildSettings;
 
 import commands.chat.*;
@@ -20,8 +39,12 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
 import java.time.Instant;
 import java.util.*;
+import java.util.List;
+
 
 public class Bot {
     private JDABuilder bot;
@@ -34,9 +57,14 @@ public class Bot {
     private Instant startTime = null;
     private boolean running = false; // Neuer Status-Tracker
     private HashMap<String, GuildSettings> guildSettingsHashMap;
+    private final LavalinkClient client;
 
     public Instant getStartTime() {
         return startTime;
+    }
+
+    public LavalinkClient getLavalinkClient() {
+        return client;
     }
 
     private static final class InstanceHolder {
@@ -56,7 +84,7 @@ public class Bot {
                 new CmdPing(),
                 new CmdGpt(),
                 //new CmdKick(),
-                //new CmdEmbed(),
+                new CmdEmbed(),
                 new CmdRollDice(),
                 //new CmdAssignTeams(),
                 //new ButtonTest(),
@@ -77,15 +105,30 @@ public class Bot {
                 new AcceptBattleshipGame(),
                 //new CmdAnnoy(),
                 new CmdSettingsChatRestricted(),
-                new CmdPlay()
+                new CmdPlay(),
+                new CmdStop(),
+                new CmdPause(),
+                new CmdResume(),
+                new CmdJoin(),
+                new CmdLeave()
         );
 
         // JDABuilder vorbereiten, aber nicht starten
         String TOKEN = System.getenv("DISCORD_BOT_TOKEN");
+        this.client = new LavalinkClient(
+                Helpers.getUserIdFromToken(TOKEN)
+        );
+        // ??
+        this.client.getLoadBalancer().addPenaltyProvider(new VoiceRegionPenaltyProvider());
+
+        registerLavalinkListeners();
+        registerLavalinkNodes();
+
         this.bot = JDABuilder
                 .createDefault(TOKEN)
                 .enableIntents(INTENTS)
                 .enableCache(CacheFlag.MEMBER_OVERRIDES, CacheFlag.VOICE_STATE, CacheFlag.ONLINE_STATUS)
+                .setVoiceDispatchInterceptor(new JDAVoiceUpdateListener(client))
                 .addEventListeners(
                         new ChatListener(),
                         new StatListener(),
@@ -94,6 +137,31 @@ public class Bot {
                         new JoinLeaveListener(),
                         new SpamListener()
                 );
+    }
+
+    private void registerLavalinkNodes() {
+        List.of(client.addNode(new NodeOptions.Builder()
+                .setName("Local-Lavalink")
+                .setServerUri(URI.create("ws://127.0.0.1:2333"))
+                .setPassword("youshallnotpass")
+                .setRegionFilter(RegionGroup.EUROPE)
+                .setHttpTimeout(5000L)
+                .build())
+        ).forEach((node) -> node.on(TrackStartEvent.class).subscribe((event) -> {
+            System.out.printf("%s: track started: %s%n", event.getNode().getName(), event.getTrack().getInfo());
+        }));
+    }
+
+    private void registerLavalinkListeners() {
+        this.client.on(ReadyEvent.class).subscribe((event) ->
+                System.out.printf("Node '%s' is ready, session id is '%s'!%n", event.getNode().getName(), event.getSessionId()));
+
+        this.client.on(StatsEvent.class).subscribe((event) ->
+                System.out.printf("Node '%s' has stats, current players: %d/%d%n", event.getNode().getName(), event.getPlayingPlayers(), event.getPlayers()));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        getInstance().start();
     }
 
     public synchronized void start() throws InterruptedException {
