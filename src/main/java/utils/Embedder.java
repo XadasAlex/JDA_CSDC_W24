e558cd0a983847da4e9d0a793e1a34734a562466
Alex Gebhart <hattie2908@gmail.com>
2025-02-03 17:42:29 +0100

    embedder: new embeds for playback

--- src/main/java/utils/Embedder.java
+++ src/main/java/utils/Embedder.java
@@ -1,10 +1,16 @@
 package utils;
 
+import dev.arbjerg.lavalink.client.player.Track;
+import dev.arbjerg.lavalink.protocol.v4.TrackInfo;
 import launcher.Bot;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
+import java.awt.*;
 import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Embedder {
     public static EmbedBuilder createMissingPermissionEmbed(Member interactingMember, String commandName) {
@@ -81,4 +87,90 @@ public class Embedder {
 
         return embed;
     }
+
+    public static EmbedBuilder createSearchResultEmbed(SlashCommandInteractionEvent event, List<Track> tracks, String searchQuery) {
+        EmbedBuilder embed = new EmbedBuilder();
+        Member interactingMember = event.getMember();
+
+        String requester = "failed";
+
+        if (interactingMember != null) {
+            requester = interactingMember.getEffectiveName();
+        }
+
+        embed.setAuthor("Using - command: Search", null, IconsGuild.LIGHTNING_ICON_URL);
+        embed.setTitle(String.format("Search results for: %s", searchQuery));
+        embed.setDescription("placeholder");
+        for (int i = 0; i < Math.min(10, tracks.size()); i++) {
+            Track track = tracks.get(i);
+            embed.addField(
+                    String.format("%02d) %s", i + 1, track.getInfo().getTitle()),
+                    String.format("Duration: %s", Helper.formatSeconds(
+                            track.getInfo().getLength() / 1000)
+                    ),
+                    false);
+        }
+        embed.setFooter(String.format("Requested by: %s", requester));
+        embed.setTimestamp(Instant.now());
+        embed.setColor(Bot.getInstance().getDefaultColor());
+
+        return embed;
+    }
+
+    public static EmbedBuilder createPlayingEmbed(SlashCommandInteractionEvent event, Track track) {
+        TrackInfo info = track.getInfo();
+        String title = info.getTitle();
+        String thumbnailUrl = info.getArtworkUrl();
+        String link = info.getUri();
+        String artist = info.getAuthor();
+        long lengthMs = info.getLength();
+
+        // Konvertiere Millisekunden in Minuten:Sekunden-Format
+        String formattedDuration = formatDuration(lengthMs);
+
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(Color.CYAN);
+        embed.setTitle(title + " by: " + artist, link);  // Titel als klickbaren Link setzen
+        embed.setThumbnail(thumbnailUrl);
+        embed.setAuthor("Now Playing", null, IconsGuild.YOUTUBE_ICON); // Icon für "Now Playing"
+        embed.addField("Estimated time until played", "00:00", true);
+        embed.addField("Track Length", formattedDuration, true);
+        embed.addField("Position in upcoming", "Next", false);
+        embed.addField("Position in queue", "00", false);
+        embed.setFooter("Requested by " + event.getUser().getName(), event.getUser().getEffectiveAvatarUrl());
+
+        return embed;
+    }
+
+    private static String formatDuration(long millis) {
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis) % 60;
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+    public static EmbedBuilder createAddedToQueueEmbed(SlashCommandInteractionEvent event, Track track) {
+        TrackInfo info = track.getInfo();
+        String title = info.getTitle();
+        String thumbnailUrl = info.getArtworkUrl();
+        String link = info.getUri();
+        String artist = info.getAuthor();
+        long lengthMs = info.getLength();
+
+        // Konvertiere Millisekunden in Minuten:Sekunden-Format
+        String formattedDuration = formatDuration(lengthMs);
+
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(Color.CYAN);
+        embed.setTitle(title + " by: " + artist, link);  // Titel als klickbaren Link setzen
+        embed.setThumbnail(thumbnailUrl);
+        embed.setAuthor("Added to Queue", null, IconsGuild.YOUTUBE_ICON); // Icon für "Now Playing"
+        embed.addField("Estimated time until played", "00:00", true);
+        embed.addField("Track Length", formattedDuration, true);
+        embed.addField("Position in upcoming", "Next", false);
+        embed.addField("Position in queue", "00", false);
+        embed.setFooter("Requested by " + event.getUser().getName(), event.getUser().getEffectiveAvatarUrl());
+
+        return embed;
+    }
+
 }
