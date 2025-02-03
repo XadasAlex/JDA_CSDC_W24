b7453f64ebbcedb207e79883e4d9755ad7e17c5b
Alex Gebhart <hattie2908@gmail.com>
2025-02-03 17:42:49 +0100

    skip functionality with trackscheduler

---
+++ src/main/java/commands/music/CmdSkip.java
@@ -0,0 +1,48 @@
+package commands.music;
+
+import audio.AudioGuildManager;
+import commands.ICommand;
+import dev.arbjerg.lavalink.client.player.Track;
+import launcher.Bot;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import utils.Embedder;
+import utils.IconsGuild;
+
+import java.util.List;
+
+public class CmdSkip implements ICommand {
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        AudioGuildManager audio = Bot.getInstance().getAudioGuildManagerById(event.getGuild().getIdLong());
+
+        if (audio == null) {
+            event.replyEmbeds(Embedder.createErrorMessage(event.getMember(), "Skip", "Audio manager not found!").build()).queue();
+            return;
+        }
+
+        // Nächsten Track abrufen und starten
+        Track nextTrack = audio.getTrackScheduler().playNextTrack();
+
+        if (nextTrack != null) {
+            event.replyEmbeds(Embedder.createBaseEmbed(event.getMember(), IconsGuild.YOUTUBE_ICON, "Skip", "You skipped to the next track", String.format("Now playing: %s", nextTrack.getInfo().getTitle())).build()).queue();
+        } else {
+            event.replyEmbeds(Embedder.createErrorMessage(event.getMember(), "Skip", "No more tracks in queue!").build()).queue();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "skip";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Skips the current song.";
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return List.of();
+    }
+}
